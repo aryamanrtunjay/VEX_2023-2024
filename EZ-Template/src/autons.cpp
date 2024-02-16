944f09179e6377604eca1380979c40cf90ae5dcd
Achintya72 <achintya.agrawal.90@gmail.com>
2024-02-16 00:38:53 -0800

    Added EZ Template

---
+++ EZ-Template/src/autons.cpp
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/////
+// For installation, upgrading, documentations and tutorials, check out our website!
+// https://ez-robotics.github.io/EZ-Template/
+/////
+
+// These are out of 127
+const int DRIVE_SPEED = 110;  
+const int TURN_SPEED = 90;
+const int SWING_SPEED = 90;
+
+///
+// Constants
+///
+void default_constants() {
+  chassis.pid_heading_constants_set(0.8, 0.01, 0);
+  chassis.pid_drive_constants_set(600, 50, 0);
+  chassis.pid_turn_constants_set(3, 0, 20);
+  chassis.pid_swing_constants_set(5, 0, 30);
+
+  chassis.pid_turn_exit_condition_set(300_ms, 3_deg, 500_ms, 7_deg, 750_ms, 750_ms);
+  chassis.pid_swing_exit_condition_set(300_ms, 3_deg, 500_ms, 7_deg, 750_ms, 750_ms);
+  chassis.pid_drive_exit_condition_set(300_ms, 1_in, 500_ms, 3_in, 750_ms, 750_ms);
+
+  chassis.slew_drive_constants_set(7_in, 80);
+}
+
+void AllAutons() {
+  chassis.pid_heading_constants_set(3, 0, 20);
+  chassis.pid_drive_constants_set(10, 0, 100);
+  chassis.pid_turn_constants_set(3, 0, 20);
+  chassis.pid_swing_constants_set(5, 0, 30);
+
+  chassis.pid_turn_exit_condition_set(300_ms, 3_deg, 500_ms, 7_deg, 750_ms, 750_ms);
+  chassis.pid_swing_exit_condition_set(300_ms, 3_deg, 500_ms, 7_deg, 750_ms, 750_ms);
+  chassis.pid_drive_exit_condition_set(300_ms, 1_in, 500_ms, 3_in, 750_ms, 750_ms);
+
+  chassis.slew_drive_constants_set(7_in, 80);
+
+  chassis.pid_drive_set(24_in, DRIVE_SPEED, true);
+  chassis.pid_wait();
+  chassis.pid_swing_set(ez::RIGHT_SWING, -90_deg, SWING_SPEED, 45);
+  chassis.pid_wait();
+
+}
